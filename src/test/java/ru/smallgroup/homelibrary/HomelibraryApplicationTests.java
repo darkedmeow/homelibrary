d6d71138075fa7344e31dfdd7d7371ec3ad3be28
Anyosi <study.all8@gmail.com>
2022-07-13 18:09:45 +0300

    init

---
+++ src/test/java/ru/smallgroup/homelibrary/HomelibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.smallgroup.homelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomelibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

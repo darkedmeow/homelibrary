7a1efcf318256e086264de11af3ed5c4b6fc8af2
Anyosi <study.all8@gmail.com>
2022-08-14 21:48:17 +0300

    error controller -> book not find

--- src/main/java/ru/smallgroup/homelibrary/controllers/ErrorController.java
+++ src/main/java/ru/smallgroup/homelibrary/controllers/ErrorController.java
@@ -5,11 +5,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.smallgroup.homelibrary.exceptions.BookNotFoundException;
 import ru.smallgroup.homelibrary.exceptions.CollectionNotFoundException;
 import ru.smallgroup.homelibrary.exceptions.UserNotFoundException;
 
 @RestControllerAdvice
 public class ErrorController {
+
+    //TODO add parent class ModelNotFoundException ?
 
     @ResponseBody
     @ExceptionHandler(UserNotFoundException.class)
@@ -22,6 +25,13 @@ public class ErrorController {
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(CollectionNotFoundException.class)
     public String collectionNotFound(CollectionNotFoundException ex) {
+        return ex.getMessage();
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(BookNotFoundException.class)
+    public String bookNotFound(BookNotFoundException ex) {
         return ex.getMessage();
     }
 
